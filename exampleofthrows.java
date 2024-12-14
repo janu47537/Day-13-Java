e576e8e47b9779a7f8dac414794f90ad5f0b9bd7
A Janani Priya <janu47537@gmail.com>
2024-12-14 15:38:14 +0530

    Add files via upload

---
+++ exampleofthrows.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class exampleofthrows{
+    public static void main(String[]args)throws ArithmeticException{
+        int a=10;
+        int b=0;
+        System.out.println(a/b);
+    }
+}
