3e567151c93e693f0e49479c04fccddf01701f7c
A Janani Priya <janu47537@gmail.com>
2024-12-14 11:19:55 +0530

    Add files via upload

---
+++ Multipleinheritance.java
@@ -0,0 +1,23 @@
+import java.util.*;
+interface a{
+    public void r();
+}
+interface b{
+    public void r1();
+
+}
+class c implements a,b{
+    public void r(){
+        System.out.println("Hello Teachers");
+    }
+    public void r1(){
+        System.out.println("Hello Professors");
+    }
+}
+public class Multipleinheritance{
+    public static void main(String[]args){
+        c h1=new c();
+        h1.r();
+        h1.r1();
+    }
+}
